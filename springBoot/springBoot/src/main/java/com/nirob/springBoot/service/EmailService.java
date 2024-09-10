8c79f10be72b62e1f69f85b1271912ed93e67118
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-09-11 05:50:05 +0600

    user mail image with error 5:43AM 09/09/2024

--- springBoot/springBoot/src/main/java/com/nirob/springBoot/service/EmailService.java
+++ springBoot/springBoot/src/main/java/com/nirob/springBoot/service/EmailService.java
@@ -1,21 +1,27 @@
 package com.nirob.springBoot.service;
 
 
-import org.springframework.mail.SimpleMailMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
+@Getter
+@Setter
 @Service
 public class EmailService {
 
     private JavaMailSender javaMailSender;
 
-    public void sendSimpleMail(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        message.setFrom("nirob@gmail.com");
-        javaMailSender.send(message);
+    public void sendSimpleMail(String to, String subject, String text) throws MessagingException {
+        MimeMessage mes = javaMailSender.createMimeMessage();
+        MimeMessageHelper massage= new MimeMessageHelper(mes,true);
+        massage.setTo(to);
+        massage.setSubject(subject);
+        massage.setText(text);
+        massage.setFrom("nirob@gmail.com");
+        javaMailSender.send(mes);
     }
 }
