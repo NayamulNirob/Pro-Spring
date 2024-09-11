b49ac606b3409d17f4589def7dbf50301a6df94a
Md. Neyamul Islam <158471735+NayamulNirob@users.noreply.github.com>
2024-09-11 19:04:28 +0600

    DB export
    7:04PM
    11/09/2024

---
+++ springBoot/DB/fromTsp.sql
@@ -0,0 +1,138 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spring
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `departments`
+--
+
+DROP TABLE IF EXISTS `departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `fac_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKehc8u1o8e9mo86oyyhkr2yakb` (`fac_id`),
+  CONSTRAINT `FKehc8u1o8e9mo86oyyhkr2yakb` FOREIGN KEY (`fac_id`) REFERENCES `facultys` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departments`
+--
+
+LOCK TABLES `departments` WRITE;
+/*!40000 ALTER TABLE `departments` DISABLE KEYS */;
+INSERT INTO `departments` VALUES (1,'Philosophy',1),(2,'Accounting',2),(3,'Biology',3),(4,'Math',3),(5,'Social Activities',4);
+/*!40000 ALTER TABLE `departments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facultys`
+--
+
+DROP TABLE IF EXISTS `facultys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `facultys` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `total_seat` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UKqij7arinyg7onyf6wngv4p177` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facultys`
+--
+
+LOCK TABLES `facultys` WRITE;
+/*!40000 ALTER TABLE `facultys` DISABLE KEYS */;
+INSERT INTO `facultys` VALUES (1,'Arts',2000),(2,'Bisness',1500),(3,'Science',2500),(4,'Social Science',1000);
+/*!40000 ALTER TABLE `facultys` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `age` int NOT NULL,
+  `birthday` date NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `dep_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKhgxp9pwct2mnqmv2y5bp7nd6j` (`dep_id`),
+  CONSTRAINT `FKhgxp9pwct2mnqmv2y5bp7nd6j` FOREIGN KEY (`dep_id`) REFERENCES `departments` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,24,'2000-02-01','Male','Rakib',1),(2,23,'2001-02-01','Male','Shakib',2),(3,23,'2002-02-03','Male','Nokib',1),(4,25,'1999-02-03','Male','Rayhan',2),(5,30,'1995-02-01','Male','Neyamul',1),(7,30,'1995-02-01','Male','Neyamul Islam',3),(8,28,'2024-09-11','Male','Towhid',3),(9,28,'2024-09-12','Male','Nirjash',3),(10,37,'2024-09-26','Male','Shabab Ahamed',4),(11,28,'2024-09-20','Male','Towhid Ahamed',2),(13,30,'2024-09-11','Male','Nafiz Shahriar Nirjash',4);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'nayamulislam@gmail.com','Neyamul','123456','679d1574-3ebc-41c2-b4ff-53a12253225b_Nid-7803455877  back.jpg'),(3,'nirobkgm@gmail.com','Nirob','123456789','e0eb169b-d1de-410e-af9d-dc061e838f3c_Nid-7803455877  back.jpg');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-11 19:03:53
