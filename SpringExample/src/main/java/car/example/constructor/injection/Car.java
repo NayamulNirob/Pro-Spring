c8cb9ee9649dd3a704e65b2043eda4c216bbd2b4
NayamulNirob <nayamulislam@gmail.com>
2025-04-30 19:14:30 +0600

    bean declaration

---
+++ SpringExample/src/main/java/car/example/constructor/injection/Car.java
@@ -0,0 +1,15 @@
+package car.example.constructor.injection;
+
+public class Car {
+
+    private Specification specification;
+
+
+    public Car(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void displayDetails() {
+        System.out.println("Car Details : "+specification.toString());
+    }
+}
