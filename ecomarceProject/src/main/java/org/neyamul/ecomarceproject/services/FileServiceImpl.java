e3d6f51924d345d902a7aeb0c20aa622584e50e9
NayamulNirob <nayamulislam@gmail.com>
2025-05-13 16:42:21 +0600

    file uupload path and file service

---
+++ ecomarceProject/src/main/java/org/neyamul/ecomarceproject/services/FileServiceImpl.java
@@ -0,0 +1,30 @@
+package org.neyamul.ecomarceproject.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public String uploadImage(String path, MultipartFile file) throws IOException {
+
+        String originalFilename = file.getOriginalFilename();
+        String randomId = UUID.randomUUID().toString();
+        String fileName = randomId.concat(originalFilename.substring(originalFilename.lastIndexOf(".")));
+        String filePath = path+ File.separator+ fileName;
+
+        File folder = new File(path);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+        return fileName;
+    }
+}
