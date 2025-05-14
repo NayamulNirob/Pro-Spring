3a355e7d81261505b26e748273b8ceee235a7365
NayamulNirob <nayamulislam@gmail.com>
2025-05-15 00:57:14 +0600

    Enhance security configuration with H2 console access and JPA support

--- securitydemo/src/main/java/org/neyamul/securitydemo/SecurityConfiguration.java
+++ securitydemo/src/main/java/org/neyamul/securitydemo/SecurityConfiguration.java
@@ -1,12 +1,13 @@
 package org.neyamul.securitydemo;
 
 
-import jakarta.websocket.Session;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,16 +20,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfigration {
+public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((
+                requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll().
+                        anyRequest().authenticated());
         http.sessionManagement(
                 session->session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS));
 //      http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+
+        http.headers(
+                headers->headers.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
