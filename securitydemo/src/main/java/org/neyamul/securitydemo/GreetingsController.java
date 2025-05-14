0ed74dc7a395f47cf920e440fbceddc27dd87bc6
NayamulNirob <nayamulislam@gmail.com>
2025-05-15 00:26:58 +0600

    Add Spring Security configuration and role-based access
    Introduced a new SecurityConfigration class to configure Spring Security with in-memory user details and role-based access control. Updated GreetingsController to include endpoints restricted to ADMIN and USER roles. Modified application.properties to include default admin credentials.

--- securitydemo/src/main/java/org/neyamul/securitydemo/GreetingsController.java
+++ securitydemo/src/main/java/org/neyamul/securitydemo/GreetingsController.java
@@ -1,7 +1,10 @@
 package org.neyamul.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
 
 @RestController
 @RequestMapping("/api/greetings")
@@ -10,5 +13,17 @@ public class GreetingsController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping("/admin")
+    public String admin() {
+        return "Hello, Admin!";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @RequestMapping("/user")
+    public String user() {
+        return "Hello, User!";
     }
 }
---
+++ securitydemo/src/main/java/org/neyamul/securitydemo/SecurityConfigration.java
@@ -0,0 +1,47 @@
+package org.neyamul.securitydemo;
+
+
+import jakarta.websocket.Session;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfigration {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(
+                session->session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS));
+//      http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin= User.withUsername("admin")
+                        .password("{noop}password")
+                        .roles("ADMIN")
+                        .build();
+          UserDetails user1= User.withUsername("user1")
+                        .password("{noop}password")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user1,admin);
+    }
+}
